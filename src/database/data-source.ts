1db0b3a4cf783a3f9fd4ad25f7a665b9ed16623b
idowuseyi <idowuseyi22@gmail.com>
2025-07-24 16:38:24 +0100

    fix: update data-source file

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -17,7 +17,11 @@ const dataSource = new DataSource({
   migrations: [process.env.DB_MIGRATIONS],
   synchronize: isDevelopment,
   migrationsTableName: 'migrations',
-  ssl: process.env.DB_SSL === 'true',
+  ssl: process.env.DB_SSL
+    ? false
+    : {
+        rejectUnauthorized: false,
+      },
 });
 export async function initializeDataSource() {
   if (!dataSource.isInitialized) {
